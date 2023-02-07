c30c745b0c5359ce5a5c1430fff8642c7eca75e3
Sam Rose <sam@floxdev.com>
2023-02-07 05:03:13 -0500

    cloudi nix flake

--- default.nix
+++ default.nix
@@ -1,44 +1,47 @@
-with import <nixpkgs> {};
+{
+  pkgs,
+  ...
+}:
+with pkgs;
+  stdenv.mkDerivation rec {
+    name = "cloudi-${version}";
+    version = "2.0.5";
 
-stdenv.mkDerivation rec {
-  name = "cloudi-${version}";
-  version = "2.0.5";
+    src = fetchzip {
+      url = "https://osdn.net/dl/cloudi/${name}.tar.gz";
+      sha256 = "sha256-Rbyt1NTjjX2uxkrp7KtC02MHMgr8Bu4iLwFdbRMppkA=";
+    };
 
-  src = fetchzip {
-    url = "https://osdn.net/dl/cloudi/${name}.tar.gz";
-    sha256 = "sha256-Rbyt1NTjjX2uxkrp7KtC02MHMgr8Bu4iLwFdbRMppkA=";
-  };
-  
-  patches = [ ./no-mkdir-logs.diff ];
+    patches = [./no-mkdir-logs.diff];
 
-  preAutoreconf = "cd src";
+    preAutoreconf = "cd src";
 
-  postFixup = ''
-    substituteInPlace $out/etc/cloudi/cloudi.conf --replace $out/lib/${name}/logs /tmp
-    substituteInPlace $out/lib/${name}/bin/cloudi --replace '$ROOTDIR/logs' /tmp
-  '';
+    postFixup = ''
+      substituteInPlace $out/etc/cloudi/cloudi.conf --replace $out/lib/${name}/logs /tmp
+      substituteInPlace $out/lib/${name}/bin/cloudi --replace '$ROOTDIR/logs' /tmp
+    '';
 
-  nativeBuildInputs = [ autoreconfHook ];
+    nativeBuildInputs = [autoreconfHook];
 
-  buildInputs = [ 
-    boost
-    erlang
-    gmp
-    ncurses
-    python3 
-    systemd
-  ];
+    buildInputs = [
+      boost
+      erlang
+      gmp
+      ncurses
+      python3
+      systemd
+    ];
 
-  configureFlags = [
-    "--enable-java-support=no"
-    "--enable-javascript-support=no"
-    "--enable-perl-support=no"   
-    "--enable-php-support=no"
-    "--enable-ruby-support=no"
-    "--enable-python-support=yes"
-    "--enable-python-c-support=no"
-    "--localstatedir=/var"
-    "--with-boost-libdir=${boost}/lib" # autoconf expects a different location
-    "--with-integration-tests=yes"
-  ];
-}
+    configureFlags = [
+      "--enable-java-support=no"
+      "--enable-javascript-support=no"
+      "--enable-perl-support=no"
+      "--enable-php-support=no"
+      "--enable-ruby-support=no"
+      "--enable-python-support=yes"
+      "--enable-python-c-support=no"
+      "--localstatedir=/var"
+      "--with-boost-libdir=${boost}/lib" # autoconf expects a different location
+      "--with-integration-tests=yes"
+    ];
+  }
